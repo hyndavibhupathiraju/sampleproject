3732f96cceb908c1e08fc4c8751a5ddeb759b4b0
hyndavi <abc@dev.org>
2020-07-18 11:22:13 +0530

    added sample class

---
+++ src/sampleproject/Sampleclass.java
@@ -0,0 +1,12 @@
+package sampleproject;
+
+public class Sampleclass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("welcome");
+		// TODO Auto-generated method stub
+
+	}
+
+}
